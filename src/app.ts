36a29b94e76038491ffdd9454b5516fd490a2c35
Miguel A. Covarrubias Reynoso <m_covar@hotmail.com>
2021-10-20 13:12:14 -0500

    Generate database json for environments and docker compose for DB creation with docker

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import orders_routes from './handlers/orders_handler';
+import products_routes from './handlers/products_handler';
+import user_routes from './handlers/users_handler';
 
 const app: express.Application = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (_req, res) => {
+  res.send('Welcome Screen');
+});
+
+products_routes(app);
+user_routes(app);
+orders_routes(app);
 
 app.listen(PORT, () => {
   console.log(`listening in port ${PORT}`);
--- src/database.ts
+++ src/database.ts
@@ -1,32 +1,29 @@
-import dotenv from "dotenv";
-import { Pool } from "pg";
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
 
 dotenv.config();
 
-const { DATABASE, TEST_DATABASE, USERNAME, PASSWORD, HOST, PORT, ENV } =
-  process.env;
+const { DATABASE, TEST_DATABASE, USERNAME, PASSWORD, HOST, DEV_PORT, TEST_PORT, ENV } = process.env;
 
-let client
-console.log(ENV);
-
-if(ENV === 'test'){
-    client = new Pool({
-        database: TEST_DATABASE,
-        user: 'postgres',
-        password: PASSWORD,
-        port: Number(PORT),
-        host: HOST
-    })
+let client;
+if (ENV === 'test') {
+  client = new Pool({
+    database: TEST_DATABASE,
+    user: 'postgres',
+    password: PASSWORD,
+    port: Number(TEST_PORT),
+    host: HOST,
+  });
 }
 
-if(ENV === 'dev'){
-    client = new Pool({
-        database: DATABASE,
-        user: 'postgres',
-        password: PASSWORD,
-        port: Number(PORT),
-        host: HOST
-    })
+if (ENV === 'dev') {
+  client = new Pool({
+    database: DATABASE,
+    user: 'postgres',
+    password: PASSWORD,
+    port: Number(DEV_PORT),
+    host: HOST,
+  });
 }
 
 export default client;
